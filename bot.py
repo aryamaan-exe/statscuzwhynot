3ec9e210b26e33715d1826de092dd4fb8f83fcd2
Aryamaan Goswamy <aryamaan.27@gmail.com>
2024-12-20 22:45:30 +0530

    -

--- bot.py
+++ bot.py
@@ -3,53 +3,96 @@ from dotenv import load_dotenv
 from discord.ext import commands
 import asyncpg
 from pylast import LastFMNetwork, SessionKeyGenerator, WSError, PERIOD_1MONTH
-import asyncio, aiohttp, logging, pycountry, random
+import asyncio, aiohttp, logging, pycountry, random, time, json
 from cryptography.fernet import Fernet
 from datetime import datetime
 import google.generativeai as genai
+from hashlib import sha256
+from openai import OpenAI
+
+def load_conversation_history():
+    if os.path.exists(conversation_history_file):
+        with open(conversation_history_file, "r") as file:
+            return json.load(file)
+    else:
+        return [
+            {"role": "system", "content": "You are an experienced programmer willing to help out noobs. You explain your work in simple terms but you never put comments in your code."},
+        ]
+
+def save_conversation_history():
+    with open(conversation_history_file, "w") as file:
+        json.dump(conversation_history, file)
+
+async def send_message_to_chatgpt(user_input):
+    conversation_history.append({"role": "user", "content": user_input})
+    response = openai.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=conversation_history,
+    )
+    assistant_reply = response.choices[0].message.content
+    conversation_history.append({"role": "assistant", "content": assistant_reply})
+    save_conversation_history()
+    return assistant_reply
+
+def split_message(message, max_length=2000):
+    return [message[i:i + max_length] for i in range(0, len(message), max_length)]
 
 async def get_country(artist):
-    url = f"https://musicbrainz.org/ws/2/artist?query={artist}&fmt=json"
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as r:
-            res = (await r.json())["artists"][0]["area"]  # Possible misidentifying an artist here
-            if res["type"] == "Country":
-                return res["name"]
-            # Annoying part starts (if not country)
-            area_id = res["id"]
-            url2 = f"https://musicbrainz.org/ws/2/area/{area_id}?fmt=json"
-            async with aiohttp.ClientSession() as session2:
-                async with session2.get(url2) as r2:
-                    ccode = (await r2.json())["iso-3166-2-codes"][0][:2]  # KR-11 -> KR
-                    country = pycountry.countries.get(alpha_2=ccode)
-                    return country.common_name  # South Korea
+    try:
+        url = f"https://musicbrainz.org/ws/2/artist?query={artist}&fmt=json"
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as r:
+                res = (await r.json())["artists"][0]["area"]  # Possible misidentifying an artist here
+                if res["type"] == "Country":
+                    return res["name"]
+                # Annoying part starts (if not country)
+                area_id = res["id"]
+                url2 = f"https://musicbrainz.org/ws/2/area/{area_id}?fmt=json"
+                async with aiohttp.ClientSession() as session2:
+                    async with session2.get(url2) as r2:
+                        ccode = (await r2.json())["iso-3166-2-codes"][0][:2]  # KR-11 -> KR
+                        country = pycountry.countries.get(alpha_2=ccode)
+                        return country.common_name  # South Korea
+    except:
+        return "Unknown"
 
 async def track_to_tup(track):
     playback_date = track.playback_date
     dt_object = datetime.strptime(playback_date, "%d %b %Y, %H:%M")
     artist = track.track.artist.name
-    try:
-        genre = track.track.artist.get_top_tags()[0].item.name # Possible that top tag is not genre but rare
-    except:
-        genre = "Unknown"
+    # try:
+    #     genre = track.track.artist.get_top_tags()[0].item.name # Possible that top tag is not genre but rare
+    # except:
+    #     genre = "Unknown"
+    genre = "Unknown"
     country = await get_country(artist)
+    id = generate_id(track)
     return (
         track.track.title,
         artist,
         track.album,
         dt_object,
         genre, 
-        country
+        country,
+        id
     )
 
-async def fetch_tracks(user, ctx):
+def generate_id(track):
+    # Prevent double counting of scrobbles by generating a unique ID
+    # If same title and artist name and playback time then it's a double scrobble
+    title = track.track.title
+    artist = track.track.artist.name
+    playback_date = str(datetime.strptime(track.playback_date, "%d %b %Y, %H:%M").timestamp())
+    return sha256((title + artist + playback_date).encode()).hexdigest()
+
+async def fetch_tracks_old(user, ctx):
     def get_recent():
         return user.get_recent_tracks(limit=10)
     logging.info("Fetching tracks")
     try:
         try:
             async with bot.pool.acquire() as conn:
-                await conn.execute(f"CREATE TABLE u{ctx.author.id} (TRACK TEXT, ARTIST TEXT, ALBUM TEXT, AT TIMESTAMP, GENRE VARCHAR(255), COUNTRY VARCHAR(255))")
+                await conn.execute(f"CREATE TABLE u{ctx.author.id} (TRACK TEXT, ARTIST TEXT, ALBUM TEXT, AT TIMESTAMP, GENRE VARCHAR(255), COUNTRY VARCHAR(255), ID CHAR(64) PRIMARY KEY)")
         except:
             async with bot.pool.acquire() as conn:
                 await conn.execute(f"DELETE FROM u{ctx.author.id}")
@@ -69,14 +112,69 @@ async def fetch_tracks(user, ctx):
     except Exception as e:
         logging.error(f"Error in fetch_tracks: {e}")
 
+
+async def fetch_tracks(user, ctx):
+    async def get_recent_tracks_in_thread(user, time_from, time_to, limit=500):
+        return await asyncio.to_thread(user.get_recent_tracks, limit=limit, time_from=time_from, time_to=time_to)
+
+    async def insert_tracks_in_batches(conn, trackobjs, ctx):
+        for i in range(0, len(trackobjs), 1000):
+            batch = trackobjs[i:i + 1000]
+            await conn.copy_records_to_table(f"u{ctx.author.id}", records=batch)
+
+    logging.info("Fetching tracks")
+    try:
+        try:
+            async with bot.pool.acquire() as conn:
+                await conn.execute(f"CREATE TABLE u{ctx.author.id} (TRACK TEXT, ARTIST TEXT, ALBUM TEXT, AT TIMESTAMP, GENRE VARCHAR(255), COUNTRY VARCHAR(255), ID CHAR(64) PRIMARY KEY)")
+        except:
+            async with bot.pool.acquire() as conn:
+                await conn.execute(f"DELETE FROM u{ctx.author.id}")
+
+        account_creation_time = user.get_unixtime_registered()
+        current_time = int(time.time())
+
+        interval_start = account_creation_time
+        interval_end = current_time
+        all_tracks = []
+
+        start = time.perf_counter()
+        while interval_start < interval_end:
+            try:
+                tracks = await get_recent_tracks_in_thread(user, interval_start, interval_end)
+                if not tracks:
+                    break
+                
+                all_tracks.extend(tracks)
+                interval_end = min(int(tracks[-1].timestamp) - 1, interval_end)
+                logging.info(f"Fetched {len(tracks)} tracks up to {interval_end}")
+                await asyncio.sleep(0.5)
+            except Exception as e:
+                logging.error(f"Error fetching tracks for interval {interval_start}-{interval_end}: {str(e)}")
+                mid_point = interval_start + (interval_end - interval_start) // 2
+                interval_end = mid_point
+        logging.info(f"Time taken: {time.perf_counter() - start}")
+
+        trackobjs = []
+        for track in all_tracks:
+            try:
+                t = await track_to_tup(track)
+                trackobjs.append(t)
+            except Exception as e:
+                logging.error(f"Error processing track {track}: {str(e)}")
+
+        async with bot.pool.acquire() as conn:
+            async with conn.transaction():
+                await insert_tracks_in_batches(conn, trackobjs, ctx)
+
+        logging.info(f"Done fetching and inserting {len(trackobjs)} tracks")
+    except Exception as e:
+        logging.error(f"Error in fetch_tracks: {str(e)}")
+
 def encrypt_sk(sk):
-    load_dotenv()
-    fern = Fernet(os.getenv("FERNET").encode())
     return fern.encrypt(sk.encode()).decode()
 
 def decrypt_sk(sk):
-    load_dotenv()
-    fern = Fernet(os.getenv("FERNET").encode())
     return fern.decrypt(sk.encode()).decode()
 
 async def send_sk(username, id, sk):
@@ -88,34 +186,33 @@ def authenticate(session_data):
     return LastFMNetwork(API, LFS, decrypt_sk(session_data[1]), session_data[0]).get_authenticated_user()
 
 async def background_update(id):
-    # Fetch the last 5 tracks and add them to table, if date matches, finish, else if all 5 are new, grab 10 more
-    # Possible optimization: Is there a way to get middle 10 tracks instead of first 15? Or get by date?
-    finished = False
-    i = 5
-    while not finished:
-        session_data = bot.sessions[id]
-        user = authenticate(session_data)
-        tracks = user.get_recent_tracks(time_from=) # NOTE: fetch the latest time from u{id} db (sort by AT because it's gonna be random order now) and use that in time_from
-        async with bot.pool.acquire() as conn:
-            recent = await conn.fetchone(f"SELECT * FROM u{id}")
-            new_tracks = []
+    session_data = bot.sessions[id]
+    user = authenticate(session_data)
+    async with bot.pool.acquire() as conn:
+        latest_time = await conn.fetchval(f"SELECT AT FROM u{id} ORDER BY AT DESC LIMIT 1")
+        tracks = user.get_recent_tracks(limit=None, time_from=int(latest_time.timestamp()))
+        new_tracks = []
+        for track in tracks:
+            new_tracks.append(await track_to_tup(track))
 
-            for track in tracks:
-                if track.playback_date == recent[3]:
-                    finished = True
-                else:
-                    new_tracks.append(track_to_tup(track))
-
-            await conn.copy_records_to_table("u"+str(id), records=new_tracks)
-            i += 10
-        
-            
-    
-    
+        # Cannot use copy_records_to_table as it will fail the entire command if any one track fails
+        for track in new_tracks:
+            try:
+                await conn.execute(f"INSERT INTO u{id} VALUES ($1, $2, $3, $4, $5, $6, $7)", *track)
+            except Exception as e:
+                pass
 
 #---------------------------------------------------------------------
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.StreamHandler() 
+    ]
+)
 load_dotenv()
+fern = Fernet(os.getenv("FERNET"))
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix=",", intents=intents)
@@ -123,6 +220,9 @@ API = os.getenv("API")
 LFS = os.getenv("LFS")
 genai.configure(api_key=os.getenv("GEM"))
 model = genai.GenerativeModel("gemini-2.0-flash-exp")
+openai = OpenAI()
+conversation_history_file = "conversation_history.json"
+conversation_history = load_conversation_history()
 
 @bot.event
 async def on_ready():
@@ -151,12 +251,12 @@ async def on_message(message):
     if message.content[0] != ",":
         return
     
+    await bot.process_commands(message)
     try:
         await background_update(message.author.id)
     except:
         pass # User does not have account and sends a message starting with ","
 
-    await bot.process_commands()
 
 #---------------------------------------------------------------------------
 
@@ -237,11 +337,11 @@ Before using SCWN, you need to agree to the following:
     await ctx.author.send("You have been authenticated. We're fetching your tracks right now. It will take a while, but we'll message you when it's done.")
     network.session_key = session_key
     user = network.get_authenticated_user()
-
+    
     try:
         tasks = [
             asyncio.create_task(send_sk(user.name, ctx.author.id, session_key)),
-            asyncio.create_task(fetch_tracks(user, ctx))
+            asyncio.create_task(fetch_tracks_old(user, ctx))
         ]
         
         await asyncio.gather(*tasks)
@@ -252,10 +352,15 @@ Before using SCWN, you need to agree to the following:
 @bot.command()
 async def recent(ctx):
     async with bot.pool.acquire() as conn:
-        tracks = await conn.fetch(f"SELECT * FROM u{ctx.author.id}")
+        tracks = await conn.fetch(f"SELECT * FROM u{ctx.author.id} ORDER BY AT DESC LIMIT 10")
+        if len(tracks) == 0:
+            session_data = bot.sessions[ctx.author.id]
+            user = authenticate(session_data)
+            await fetch_tracks(user, ctx)
+            tracks = await conn.fetch(f"SELECT * FROM u{ctx.author.id} ORDER BY AT DESC LIMIT 10")
     s = ""
     for track in tracks:
-        # TRACK ARTIST ALBUM AT GENRE COUNTRY
+        # TRACK ARTIST ALBUM AT GENRE COUNTRY ID
         s += track[0] + " - " + track[1] + "\n"
     e = discord.Embed(
         title="Your Recent Tracks",
@@ -269,11 +374,17 @@ async def roast(ctx):
     session_data = bot.sessions[ctx.author.id]
     user = authenticate(session_data)
     top_artists = random.sample(list(map(lambda x: x.item.name, user.get_top_artists(period=PERIOD_1MONTH, limit=20))), 3)
+    start = time.time()
     response = model.generate_content(f"Give me 5 paragraphs roasting the music taste of someone who likes {', '.join(top_artists)}.")
+    end = time.time()
+    
     e = discord.Embed(
         title="Your Roast",
-        description=response.text
+        description=response.text,
+        color=0xe65912,
     )
+    e.set_footer(text=f"Response took {round((end - start), 2)} seconds.")
+
     await message.edit(content="", embed=e)
 
 @bot.command()
@@ -282,12 +393,50 @@ async def praise(ctx):
     session_data = bot.sessions[ctx.author.id]
     user = authenticate(session_data)
     top_artists = random.sample(list(map(lambda x: x.item.name, user.get_top_artists(period=PERIOD_1MONTH, limit=20))), 3)
+    start = time.time()
     response = model.generate_content(f"Give me 5 paragraphs praising the music taste of someone who likes {', '.join(top_artists)}.")
+    end = time.time()
+    
     e = discord.Embed(
         title="Your Praise",
-        description=response.text
+        description=response.text,
+        color=0x27d6c2
     )
+    e.set_footer(text=f"Response took {round((end - start), 2)} seconds.")
     await message.edit(content="", embed=e)
+
+@bot.command(aliases=["rec"])
+async def recommend(ctx, *, picks=None):
+    message = await ctx.send("Generating Recommendations...")
+    
+    if picks == None:
+        session_data = bot.sessions[ctx.author.id]
+        user = authenticate(session_data)
+        picks = ", ".join(random.sample(list(map(lambda x: x.item.title, user.get_top_albums(period=PERIOD_1MONTH, limit=20))), 3))
+    
+    start = time.time()
+    response = model.generate_content(f"Recommend 3 albums (going from less obscure from more obscure, don't mention this fact in your response) to someone who likes {picks}.")
+    end = time.time()
+    
+    e = discord.Embed(
+        title="Your Recommendations",
+        description=response.text,
+        color=0x1cba1c
+    )
+    e.set_footer(text=f"Response took {round((end - start), 2)} seconds.")
+    
+    await message.edit(content="", embed=e)
+
+
+@bot.command()
+async def ai(ctx, *, prompt):
+    start = time.time()
+    response_content = await send_message_to_chatgpt(prompt)
+    split_response = split_message(response_content)
+    for part in split_response:
+        await ctx.send(part)
+    end = time.time()
+    await ctx.send(f"Response took `{round((end - start), 2)}` seconds.")
 
 
 bot.run(os.getenv("BOT"))
